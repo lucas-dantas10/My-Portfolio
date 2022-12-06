e8fdbafb5504b659741918931ac847e01648f044
lucas <lucas.dantas.nogueira@gmail.com>
2022-12-06 10:26:39 -0300

    Renomeando constantes e deixando o código mais limpo

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
 const url = "https://api.github.com/users/lucas-dantas10/repos";
 
-const nomeCurso = document.getElementById('name-course');
-const nameCourse = document.getElementById('course-name');
+const listCourse = document.getElementById('name-course');
+const aluraTracker = document.getElementById('course-name');
 
 const nomeRepositorio = fetch(url)
     .then(res => {
         return res.json();
     })
     .then(response => {
-
-        console.log(response);
-
         response.forEach(res => {
             if (res.id === 550959984) {
                 let nameRepos = res.name;
-
-                nameCourse.textContent = nameRepos;
+                aluraTracker.textContent = nameRepos;
 
             } else if (res.id === 523355110) {
                 let nameRepos = res.name;
-
-                nomeCurso.textContent = nameRepos;
+                listCourse.textContent = nameRepos;
             }
         });
     });
